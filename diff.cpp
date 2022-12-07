f363f874ec09f9fd1d7199eb6624ed4775c4fb68
Umer <workumer9@gmail.com>
2022-12-07 20:58:36 +0500

    2nd Commit

---
+++ diff.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<" Enter numer 1 : \n";
+    cin>>a;
+    cout<<" Enter number 2 : \n";
+    cin>>b;
+
+    cout<<" Difference : "<< a-b<<endl;
+
+
+    return 0;
+}
