bce5b519cd9ea50560665806e12c22c78fc8dca1
Umer <workumer9@gmail.com>
2022-12-07 22:38:07 +0500

    Division  feature added

---
+++ div.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<" Enter no 1 : ";
+    cin>>a;
+    cout<<" Enter no 2 : ";
+    cin>>b;
+
+    cout<<" DIV : "<< a/b<<endl;
+
+
+    return 0;
+}
